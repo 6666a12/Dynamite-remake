94b538d277486e71090a2d3e2100e3c80de985fc
Lunar1aa <fzm123fzms@163.com>
2026-05-22 00:26:57 +0800

    gitignore

--- android/app/build/intermediates/cxx/Debug/3u371yw1/logs/arm64-v8a/build_command_targets.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-"C:\\Users\\fzm12\\AppData\\Local\\Android\\Sdk\\cmake\\4.1.2\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\fzm12\\Desktop\\Dynamite app\\dynamite-rebuild\\android\\app\\.cxx\\Debug\\3u371yw1\\arm64-v8a" ^
-  gamecore ^
-  main
--- android/app/proguard-rules.pro
+++
@@ -1,4 +0,0 @@
-# ProGuard rules for Dynamite
-# SDL3 classes
--keep class org.libsdl.app.** { *; }
--keep class com.dynamite.** { *; }
